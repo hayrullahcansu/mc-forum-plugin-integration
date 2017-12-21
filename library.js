cfca4e1f808d9be4d7017b1473c1bb6df2992694
Hayrullah Cansu <hayrullah.cansu@gmail.com>
2017-12-21 16:33:02 +0300

    temp

--- library.js
+++ library.js
@@ -2,7 +2,7 @@
 
 var controllers = require('./lib/controllers'),
 	plugin = {};
-var middleware = module.parent.require('./middleware');	
+var middleware = module.parent.require('./middleware');
 var User = module.parent.require('./user');
 
 plugin.init = function (params, callback) {
@@ -10,17 +10,17 @@ plugin.init = function (params, callback) {
 		hostMiddleware = params.middleware,
 		hostControllers = params.controllers;
 	var middlewares = [
-		middleware.maintenanceMode, 
+		middleware.maintenanceMode,
 		middleware.registrationComplete,
 		middleware.redirectToAccountIfLoggedIn
 	]
-	router.get('/check_session',middlewares,controllers.CheckSession);
+	router.get('/check_session', controllers.CheckSession);
 
 	//will add hostMiddleware.applyCSRF,
-	router.post('/api/AuthFromBaseSite',middlewares,controllers.AuthFromBaseSite);
-	router.get('/api/AuthFromBaseSite',middlewares,controllers.AuthFromBaseSite);	
-	router.get('/setUserAccessKey',controllers.setUserAccessKey);
-	router.get('/getUserAccessKey',controllers.getUserAccessKey);
+	router.post('/api/AuthFromBaseSite', controllers.AuthFromBaseSite);
+	router.get('/api/AuthFromBaseSite', controllers.AuthFromBaseSite);
+	router.get('/setUserAccessKey', controllers.setUserAccessKey);
+	router.get('/getUserAccessKey', controllers.getUserAccessKey);
 	callback();
 };
 
